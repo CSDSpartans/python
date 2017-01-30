94b54235ba55d8e1cce370ef1bc58ca6e07d6886
William Muhlbach <applesarefum>
2017-01-30 13:02:14 -0500

    save

--- magic8/ballofsalsa.py
+++ magic8/ballofsalsa.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python3
 import random
 answers = ["Salsa", "Salsa!", "SALSA","DID SOME BODY SAY SALSA?!"]
-def game():
-    print("Welcome to the Ball of Salsa!")
-    if 'salsa' in input("What is your question? >>>").lower():
-        print('''███████╗ █████╗ ██╗     ███████╗ █████╗ ██╗
-██╔════╝██╔══██╗██║     ██╔════╝██╔══██╗██║
-███████╗███████║██║     ███████╗███████║██║
-╚════██║██╔══██║██║     ╚════██║██╔══██║╚═╝
-███████║██║  ██║███████╗███████║██║  ██║██╗
-╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝''')
-    else:
-        print=random.choice(answers)
-
-try:
-    while True:
-        game()
-    
-except(KeyboardInterrupt):
-    print("Thanks for playing!")
-    exit
-
+print("Welcome to the Ball of Salsa!")
+if 'salsa' in input("What is your question? >>>").lower():
+    print("███████╗ █████╗ ██╗     ███████╗ █████╗ ██╗")
+    print("██╔════╝██╔══██╗██║     ██╔════╝██╔══██╗██║")
+    print("███████╗███████║██║     ███████╗███████║██║")
+    print("╚════██║██╔══██║██║     ╚════██║██╔══██║╚═╝")
+    print("███████║██║  ██║███████╗███████║██║  ██║██╗")
+    print("╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝")
+else:
+    print=random.choice(answers)
